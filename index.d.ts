88cc73a46caa732d8ece70f41e88f27281029787
Alexey Yak <yak.alexey@outlook.com>
2025-05-30 11:30:02 +0300

    fix: correct TypeScript module declaration

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { Component } from 'react'
+import { CountDownProps, CountDownState } from './countdown.types'
+
+export { CountDownProps, CountDownState }
+export default class CountDown extends Component<CountDownProps, CountDownState> {}
